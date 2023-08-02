729d075dc6118a471790a5eabe53477ba735547d
SergShcherbina <1990sergik27@gmail.com>
2023-08-02 11:39:09 +0300

    feat: add component singUp

---
+++ src/components/auth/sing-up/singUp.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { SingUp } from './singUp.tsx'
+
+const meta = {
+  title: 'Auth/SingUp',
+  component: SingUp,
+  tags: ['autodocs'],
+} satisfies Meta<typeof SingUp>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {}
