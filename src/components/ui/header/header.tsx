b560994bdf984a98a75faa58812932035028ca82
SergShcherbina <1990sergik27@gmail.com>
2023-12-19 00:29:05 +0300

    feat: fix logo & adaptation header

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,6 +1,6 @@
 import { ComponentPropsWithoutRef, FC } from 'react'
 
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 import { UserType } from '../../../services/auth'
 import { Avatar } from '../avatar'
@@ -13,7 +13,6 @@ import personIcon from '../drop-down-menu/img/personOutline.svg'
 import { Typography } from '../typography'
 
 import s from './header.module.scss'
-import { Logo } from './img/logo.tsx'
 
 type HeaderType = {
   userData?: UserType | null
@@ -21,39 +20,48 @@ type HeaderType = {
 } & ComponentPropsWithoutRef<'header'>
 
 export const Header: FC<HeaderType> = ({ userData, onLogout }) => {
+  const navigate = useNavigate()
+  const profileHandler = () => {
+    navigate('profile')
+  }
+
   return (
     <div className={`${s.wrapperHeader}`}>
-      <Link to={'/'} className={s.linkHome}>
-        <div className={s.logoContainer}>
-          <Logo className={s.logo} />
-        </div>
-      </Link>
-      {userData ? (
-        <div className={s.info}>
-          <Typography variant={'subtitle1'} className={s.name}>
-            {userData.name}
-          </Typography>
-          <DropDownMenu trigger={<Avatar src={userData.avatar} />}>
-            <DropDownMenuHeader
-              userPhoto={userData.avatar}
-              userName={userData.name}
-              userEmail={userData.email}
-            />
-            <DropDownMenuItem
-              className={s.myProfile}
-              as={'a'}
-              href={'/profile'}
-              icon={personIcon}
-              text={'My Profile'}
-            />
-            <DropDownMenuItem icon={logOutIcon} text={'Sign Out'} onSelect={onLogout} />
-          </DropDownMenu>
-        </div>
-      ) : (
-        <Button as={'a'} href={'/login'}>
-          Sign In
-        </Button>
-      )}
+      <div className={s.container}>
+        <Link to={'/'} className={s.linkHome}>
+          <div className={s.logoContainer}>
+            <h2 className={s.logo}>
+              {/* eslint-disable-next-line react/no-unescaped-entities */}
+              <span className={s.bracket}>[</span> app - cards <span className={s.bracket}>]</span>
+            </h2>
+          </div>
+        </Link>
+        {userData ? (
+          <div className={s.info}>
+            <Typography variant={'subtitle1'} className={s.name}>
+              {userData.name}
+            </Typography>
+            <DropDownMenu trigger={<Avatar src={userData.avatar} />}>
+              <DropDownMenuHeader
+                userPhoto={userData.avatar}
+                userName={userData.name}
+                userEmail={userData.email}
+              />
+              <DropDownMenuItem
+                className={s.myProfile}
+                onSelect={profileHandler}
+                icon={personIcon}
+                text={'My Profile'}
+              />
+              <DropDownMenuItem icon={logOutIcon} text={'Sign Out'} onSelect={onLogout} />
+            </DropDownMenu>
+          </div>
+        ) : (
+          <Button as={'a'} href={'/login'}>
+            Sign In
+          </Button>
+        )}
+      </div>
     </div>
   )
 }
