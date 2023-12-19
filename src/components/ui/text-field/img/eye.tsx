9064a7e5d20a1d9baf8b432c0527e4758d511232
SergShcherbina <1990sergik27@gmail.com>
2023-12-19 21:00:00 +0300

    feat: fix icons for TextField, fix story search

--- src/components/ui/text-field/img/eye.tsx
+++ src/components/ui/text-field/img/eye.tsx
@@ -2,8 +2,8 @@ import { SVGProps, Ref, forwardRef, memo } from 'react'
 
 const Eye = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
   <svg
-    width="20"
-    height="20"
+    width="24"
+    height="24"
     viewBox="0 0 20 20"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
